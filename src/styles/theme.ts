d27cb616e54ef7f2543c42c9828f2679add5be35
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2022-04-13 08:26:34 -0300

    feat: create styled-components theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,21 @@
+import { DefaultTheme } from 'styled-components'
+
+const light: DefaultTheme = {
+  borderRadius: '5px',
+
+  colors: {
+    primary: 'cyan',
+    secondary: 'magenta',
+  },
+}
+
+const dark: DefaultTheme = {
+  borderRadius: '5px',
+
+  colors: {
+    primary: 'cyan',
+    secondary: 'magenta',
+  },
+}
+
+export { light, dark }
